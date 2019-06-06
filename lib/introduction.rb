866599df26e10a25e272c190f7c05c615896c9ed
Valerie Manuel <valerie.manuel11@gmail.com>
2019-06-06 16:28:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_name (name, language)
-  puts "Hi, my name is (name) and I am learning to program in (language)."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_name (Valerie, Ruby)
 introduction_with_name (Valerie, Ruby)
